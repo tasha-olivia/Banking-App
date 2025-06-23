1935f7914fc391142ed94b0e107810638a17afc7
Olivia <tashaolivia47@gmail.com>
2025-06-23 01:07:47 +0100

    created render dabase connection

--- banking_app/settings.py
+++ banking_app/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = "banking_app.wsgi.application"
 #     }
 # }
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
